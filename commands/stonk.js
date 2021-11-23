2f5cedb3b7c871eb0ab595d1265c3078cc340116
bglaszcz <bglaszcz@gmail.com>
2021-11-23 08:47:59 -0600

    code cleanup

--- commands/stonk.js
+++ commands/stonk.js
@@ -17,27 +17,56 @@ module.exports = {
 
             var $ = cheerio.load(body);
 
+            let date_ob = new Date().getHours();
+            let minutes = new Date().getMinutes();
+
             var company = $(`h1[data-reactid='7']`).first().text().toString();
-            var cur_price = $(`#quote-market-notice`).parent().children('span').first().text();
-            var change = $(`#quote-market-notice`).parent().children('span').eq(1).first().text();
             var volume = $(`[data-test='TD_VOLUME-value']`).first().text().toString();
             var volume_avg = $(`[data-test='AVERAGE_VOLUME_3MONTH-value']`).first().text().toString();
 
+            // if (date_ob < 16) {
+            //     var cur_price = $(`#quote-market-notice`).parent().children('span').first().text();
+            //     var change = $(`#quote-market-notice`).parent().children('span').eq(1).first().text();
+            // } else {
+            //     var cur_price = $(`span[data-reactid='52']`).first().text().toString();
+            //     var change = $(`span[data-reactid='55']`).first().text().toString();
+            // }
+
+            if (date_ob <= 8 && minutes < 30) {
+                var priceHeader = "Price (Pre-Market)"
+                var changeHeader = "Change (Pre-Market)"
+                var embedColor = "#fa7b62"
+                var cur_price = $(`span[data-reactid='52']`).first().text().toString();
+                var change = $(`span[data-reactid='55']`).first().text().toString();
+            } else if (date_ob > 14 ) {
+                var priceHeader = "Price (After Hours)"
+                var changeHeader = "Change (After Hours)"
+                var embedColor = "#ECC1B2"
+                var cur_price = $(`span[data-reactid='52']`).first().text().toString();
+                var change = $(`span[data-reactid='55']`).first().text().toString();
+            } else {
+                var priceHeader = "Price"
+                var changeHeader = "Change"
+                var embedColor = '#0099ff'
+                var cur_price = $(`#quote-market-notice`).parent().children('span').first().text();
+                var change = $(`#quote-market-notice`).parent().children('span').eq(1).first().text();
+            }
+
             if (company) {
-                // message.channel.send(`${company}: ${cur_price} ${change} (vol:${volume})`);
                 let exampleEmbed = new Discord.MessageEmbed()
-                .setColor('#0099ff')
+                .setColor(embedColor)
                 .setURL(URL)
                 .setTitle(company)
                 .addFields(
-                    {name: 'Price', value: cur_price, inline: true},
-                    {name: 'Change', value: change, inline: true},
-                    { name: '\u200B', value: '\u200B', inline: true},
+                    {name: priceHeader, value: cur_price, inline: true},
+                    {name: changeHeader, value: change, inline: true},
+                    {name: '\u200B', value: '\u200B', inline: true},
                     {name: '24 Hour Volume', value: volume, inline: true},
-                    {name: 'Average Volume', value: volume_avg, inline: true}
+                    {name: 'Average Volume', value: volume_avg, inline: true},
                 );    
                 message.channel.send(exampleEmbed);
-            } else {
+            }
+            else {
             message.channel.send(`No stonk with the ticker ${args}.`)
             }
             
--- commands/test.js
+++ commands/test.js
@@ -6,7 +6,7 @@ const { first } = require('cheerio/lib/api/traversing');
 const Discord = require("discord.js");
 
 module.exports = {
-    name: `stonk`,
+    name: `test`,
     description: 'Show stock price.',
     async execute(message, args) {
         let URL = `https://finance.yahoo.com/quote/${args}`;
@@ -18,16 +18,20 @@ module.exports = {
             var $ = cheerio.load(body);
 
             let date_ob = new Date().getHours();
-            console.log(date_ob);
 
             var company = $(`h1[data-reactid='7']`).first().text().toString();
-            var cur_price = $(`#quote-market-notice`).parent().children('span').first().text();
-            var change = $(`#quote-market-notice`).parent().children('span').eq(1).first().text();
             var volume = $(`[data-test='TD_VOLUME-value']`).first().text().toString();
             var volume_avg = $(`[data-test='AVERAGE_VOLUME_3MONTH-value']`).first().text().toString();
 
-            if (company) {
-                // message.channel.send(`${company}: ${cur_price} ${change} (vol:${volume})`);
+            if (date_ob < 16) {
+                var cur_price = $(`#quote-market-notice`).parent().children('span').first().text();
+                var change = $(`#quote-market-notice`).parent().children('span').eq(1).first().text();
+            } else {
+                var cur_price = $(`span[data-reactid='52']`).first().text().toString();
+                var change = $(`span[data-reactid='55']`).first().text().toString();
+            }
+
+            if (company && date_ob < 16) {
                 let exampleEmbed = new Discord.MessageEmbed()
                 .setColor('#0099ff')
                 .setURL(URL)
@@ -37,10 +41,24 @@ module.exports = {
                     {name: 'Change', value: change, inline: true},
                     { name: '\u200B', value: '\u200B', inline: true},
                     {name: '24 Hour Volume', value: volume, inline: true},
-                    {name: 'Average Volume', value: volume_avg, inline: true}
+                    {name: 'Average Volume', value: volume_avg, inline: true},
                 );    
                 message.channel.send(exampleEmbed);
-            } else {
+            } else if (company){
+                let exampleEmbed = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setURL(URL)
+                .setTitle(company)
+                .addFields(
+                    {name: 'Price AH', value: cur_price, inline: true},
+                    {name: 'Change AH', value: change, inline: true},
+                    { name: '\u200B', value: '\u200B', inline: true},
+                    {name: '24 Hour Volume', value: volume, inline: true},
+                    {name: 'Average Volume', value: volume_avg, inline: true},
+                );    
+                message.channel.send(exampleEmbed);
+            }
+            else {
             message.channel.send(`No stonk with the ticker ${args}.`)
             }
             
