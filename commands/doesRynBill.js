85791be43fcf3f1f1dfa153f61bf4f2c19a15641
bglaszcz <bglaszcz@gmail.com>
2022-04-26 15:18:01 -0500

    general updates

--- commands/doesRynBill.js
+++ commands/doesRynBill.js
@@ -2,6 +2,15 @@ module.exports = {
     name: 'doesrynbill',
     description: 'doesrynbill!',
     execute(message, args) {
-        message.channel.send(`I looked into it, he does not.`);
+
+        var rando = Math.floor(Math.random() * 100) + 1;
+        var today = new Date(Date.now());
+
+        if (rando == 69) {
+            message.channel.send(`LET IT BE KNOWN! On the Lords day of ${today.toLocaleDateString()} ryn did a bill.`)
+        }
+        else {
+            message.channel.send(`I looked into it, he does not.`);
+        }
     },
 }
