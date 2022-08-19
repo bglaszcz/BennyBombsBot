626b61a6d0951a8e35279536336557b91601ed05
bglaszcz <bglaszcz@gmail.com>
2022-08-19 14:26:31 -0500

    stonk update

--- commands/stonk.js
+++ commands/stonk.js
@@ -13,22 +13,28 @@ module.exports = {
         
         const result = await yahooFinance.quoteSummary(`${args}`);
         const company = result.price.shortName;
-        const cur_price = result.price.regularMarketPrice.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
-        const change = (result.price.regularMarketChangePercent * 100).toFixed(2)+"%";
+        //const cur_price = result.price.regularMarketPrice.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+        //const change = (result.price.regularMarketChangePercent * 100).toFixed(2)+"%";
         const volume = result.price.regularMarketVolume.toLocaleString("en-US");
         const volume_avg = result.summaryDetail.averageVolume.toLocaleString("en-US");
 
-        if (date_ob <= 8 && minutes < 30) {
+        if (date_ob <= 8 && minutes < 30) {t
             var priceHeader = "Price (Pre-Market)"
             var changeHeader = "Change (Pre-Market)"
+            var cur_price = result.price.preMarketPrice.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+            var change = (result.price.preMarketChangePercent * 100).toFixed(2)+"%";
             var embedColor = "#FFAE42"
         } else if (date_ob > 14 ) {
             var priceHeader = "Price (After Hours)"
             var changeHeader = "Change (After Hours)"
+            var cur_price = result.price.postMarketPrice.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+            var change = (result.price.postMarketChangePercent * 100).toFixed(2)+"%";
             var embedColor = "#FF5349"
         } else {
             var priceHeader = "Price"
             var changeHeader = "Change"
+            var cur_price = result.price.regularMarketPrice.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+            var change = (result.price.regularMarketChangePercent * 100).toFixed(2)+"%";
             var embedColor = '#0000FF'
         }
 
