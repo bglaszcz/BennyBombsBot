a4210b40366b8fbbdc77416882300d7a8ba8d16f
Ben Glaszcz <bglaszcz@gmail.com>
2023-07-09 21:57:38 -0500

    not sure where I'm at

---
+++ commands/b3.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
+const openai = require('../chatgpt.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('b3')
+		.setDescription('B3 is alive'),
+	async execute(interaction) {
+		let 
+	},
+};
